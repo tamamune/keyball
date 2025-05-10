86114430ff71da31f2af42f88ea47db13fd47563
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2025-05-11 00:17:41 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 2
+#define AUTO_MOUSE_DEFAULT_LAYER 3
 
 // レイヤー数
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
