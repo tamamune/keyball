3b7293659b2855050d7ca05c441ae5ed765d6d5a
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2025-05-12 14:30:57 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via2/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 1);
+    keyball_set_scroll_mode(get_highest_layer(state) == 3);
 
     // レイヤー毎のカラー指定 MAGENTA BLUE YELLOW RED WHITE GREEN
     state = update_tri_layer_state(state, 1, 2, 3);
