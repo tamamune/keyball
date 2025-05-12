b6252e0d4bdd970ece364eb1f55af137c2d81174
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2025-05-12 11:33:59 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via2/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX  , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                          KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                          KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_ENT   ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , LT(1,KC_RBRC),            LT(2,KC_NUHS), KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-    KC_LCTL  , KC_LWIN  , KC_LALT  , TO(2)    , LT(1,KC_LNG2) , LT(2,KC_SPC) , LT(3,KC_LNG1) ,  LT(2,KC_ENT) , LT(1,KC_SPC) ,XXXXXXX , XXXXXXX , XXXXXXX , TO(3)    , TO(4)
+    KC_LCTL  , KC_LWIN  , KC_LALT  , TO(1)    , LT(1,KC_LNG2) , LT(2,KC_SPC) , LT(3,KC_LNG1) ,  LT(2,KC_ENT) , LT(1,KC_SPC) ,XXXXXXX , XXXXXXX , XXXXXXX , TO(2)    , TO(4)
   ),
 
   [1] = LAYOUT_universal(
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , _______  ,                                  RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , _______  ,                                  CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE , KBC_RST  ,
     _______  , _______  , SCRL_DVD , SCRL_DVI , SCRL_MO  , SCRL_TO  , EE_CLR   ,            EE_CLR   , KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   , _______  , _______  ,
-    QK_BOOT  , _______  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  ,            _______  , KC_BSPC  , _______  , _______  , _______  , _______  , TO(0)
+    QK_BOOT  , _______  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  ,            _______  , KC_BSPC  , _______  , _______  , _______  , _______  , QK_BOOT
   ),
   [4] = LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
