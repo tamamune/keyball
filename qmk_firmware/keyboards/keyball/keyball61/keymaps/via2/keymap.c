6f2c037960766b0b0960afb9ddce1162c1afedb5
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2025-05-17 10:28:48 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via2/keymap.c
@@ -2,14 +2,12 @@
 
 #include "quantum.h"
 
-//MO(4) TT(4)
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                          KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_BSPC  ,
-    TG(4)    , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                          KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                          KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_ENT   ,
+    TG(4,KC_TAB)    , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                          KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
+    KC_CAPS   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                          KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_ENT   ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , LT(1,KC_RBRC),            LT(2,KC_NUHS), KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
     KC_LCTL  , KC_LWIN  , KC_LALT  , TG(1)    , LT(1,KC_LNG2) , LT(2,KC_SPC) , LT(3,KC_LNG1) ,  LT(2,KC_ENT) , LT(1,KC_SPC) ,XXXXXXX , XXXXXXX , XXXXXXX , TG(2)    , TG(3)
   ),
@@ -51,7 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
-
+    return state;
+  
     // レイヤー毎のカラー指定 MAGENTA BLUE YELLOW RED WHITE GREEN
     state = update_tri_layer_state(state, 1, 2, 3);
     uint8_t layer = biton32(state);
