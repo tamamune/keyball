1f61a4fcdc1aca79cd90d5e911760fec995deb39
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2025-05-17 16:38:15 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via2/keymap.c
@@ -76,11 +76,12 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 #ifdef OLED_ENABLE
 
-#    include "lib/oledkit/oledkit.h"
+#include "lib/oledkit/oledkit.h"
+#include "custom_oled.c"
 
 void oledkit_render_info_user(void) {
-    keyball_oled_render_keyinfo();
-    keyball_oled_render_ballinfo();
-    keyball_oled_render_layerinfo();
+    // keyball_oled_render_keyinfo();
+    // keyball_oled_render_ballinfo();
+    // keyball_oled_render_layerinfo();
+    keyball_oled_render_mymain();
 }
-#endif
