daa20a54fdf2925b6be5301bbe282e517a94198f
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2025-05-17 20:09:48 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via2/keymap.c
@@ -50,13 +50,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // clang-format on
 
+#define LAYER_LED_ENABLE
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
     return state;
 }
 
-// #ifdef OLED_ENABLE
+#ifdef OLED_ENABLE
 
 // https://mazcon.hatenablog.com/entry/2023/11/20/022521
 #include "lib/oledkit/oledkit.h"
