10330f110a4d049953e499d8747465cebf61b591
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2025-07-13 22:16:37 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via3/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via3/config.h
@@ -1,23 +1,3 @@
-/*
-This is the c configuration file for the keymap
-
-Copyright 2022 @Yowkees
-Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
@@ -27,13 +7,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_SNAKE
 //#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //#    define RGBLIGHT_EFFECT_RGB_TEST
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
 #define TAP_CODE_DELAY 5
-
+// レイヤー数
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+// キー長押し時間 ミリ秒
+#define TAPPING_TERM 180
+// AML有効化
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 2
+// 切り替えるマウスレイヤー番号を指定
+#define AUTO_MOUSE_DEFAULT_LAYER 4
+// マウスが止まってから元のレイヤーに戻るまでの時間(ms)
+#define AUTO_MOUSE_TIME 500
+// マウス速度 (default: 500)
+#define KEYBALL_CPI_DEFAULT 1100
+// スクロール速度 (default: 4)
+#define KEYBALL_SCROLL_DIV_DEFAULT 5
